4915834b9433becc4b10f876b492e4e9e5db0a9c
Colin Rood <crrood@gmail.com>
2014-04-24 21:15:41 -0700

    first steps
    created Driver class to control browser
    basic decision tree

--- doge.rb
+++ doge.rb
@@ -1,31 +1,79 @@
 require "selenium-webdriver"
 
-# load driver
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://doge2048.com"
+class Driver
 
-# get outer div to send keys to
-container = driver.find_element(:class, "game-container")
+	# load selenium and target webpage
+	def initialize(address)
+		@selenium = Selenium::WebDriver.for :firefox
+		@selenium.navigate.to(address)
 
-# load board state
-tiles = driver.find_elements(:class, "tile")
-tile_matrix = [[],[],[],[]]
+		# get outer div to send keys to
+		@container = @selenium.find_element(:class, "game-container")
+	end
 
-tiles.map do |tile|
-	
-	# read position data from class labels
-	class_split = tile.attribute("class").split(" ")
-	value = class_split[1].split("-")[1]
-	position = [class_split[2].split("-")[2].to_i - 1, class_split[2].split("-")[3].to_i - 1]
-	
-	# load into 2-d array
-	tile_matrix[position[0]][position[1]] = value
-	
+	# load board state from DOM
+	def read_tiles
+
+		board_matrix = []
+		(0..3).each do |x|
+			board_matrix[x] = []
+			(0..3).each do |y|
+				board_matrix[x][y] = 0
+			end
+		end
+
+		tiles = @selenium.find_elements(:class, "tile")
+		tiles.map do |tile|
+			
+			# read position data from class labels
+			class_split = tile.attribute("class").split(" ")
+			value = class_split[1].split("-")[1].to_i
+			position = [class_split[2].split("-")[2].to_i - 1, class_split[2].split("-")[3].to_i - 1]
+			
+			# load into 2-d array
+			board_matrix[position[0]][position[1]] = value
+
+		end
+
+		return board_matrix
+	end
+
+	# "ground" method to send basic selenium commands to the driver
+	def send_keys(keys)
+		@container.send_keys(keys)
+	end
+
 end
 
-puts tile_matrix.to_s
+def find_optimal_move(board_state)
 
-# miscellaneous jiberish that I don't want to forget
-sleep(3)
-container.send_keys :arrow_left
-driver.quit
+	# check if match can be made down
+	width = board_state.length
+	(0..(width-1)).each do |x|
+		i = 1
+		point = nil
+		while i <= width
+			if point && board_state[x][width-i] == point
+				return :arrow_down
+			elsif board_state[x][width-i] != 0
+				point = board_state[x][width-i]
+			end
+			i += 1
+		end
+	end
+
+	return (if Random.rand > 0.5 then :arrow_right else :arrow_left end)
+
+end
+
+driver = Driver.new "http://gabrielecirulli.github.io/2048/"
+
+# 4x4 matrix to hold board state
+tile_matrix = driver.read_tiles
+
+150.times do
+	driver.send_keys(find_optimal_move(driver.read_tiles))
+	sleep(0.1)
+end
+
+#driver.send_keys(:arrow_left)
