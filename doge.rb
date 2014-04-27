dddd6ada4e678a5ad45c524dfe79bec8e6e18dd3
Colin Rood <crrood@gmail.com>
2014-04-27 11:53:59 -0700

    optimizing read_tiles
    load the HTML source (once) and parse as a string instead of hitting the
    DOM +/- 16 times per move

--- doge.rb
+++ doge.rb
@@ -21,17 +21,17 @@ class Driver
 	def read_tiles
 
 		# initialize 4x4 array
-		board_matrix = Array.new([[0, 0, 0, 0],
+		board_matrix = [[0, 0, 0, 0],
 						[0, 0, 0, 0],
 						[0, 0, 0, 0],
-						[0, 0, 0, 0]])
+						[0, 0, 0, 0]]
 
+		# scan source HTML of target page for tile positions / values
 		# tile classes are of the form "tile tile-{value} tile-{x}-{y} {tile-modifier}"
-		tiles = get_elements_by_class("tile")
-		tiles.map do |tile|
+		@selenium.page_source.scan(/tile [^"]*/).map do |tile|
 			
 			# read position data from class labels
-			class_split = tile.attribute("class").split(" ")
+			class_split = tile.split
 			value = class_split[1].split("-")[1].to_i
 			position = [class_split[2].split("-")[2].to_i - 1, class_split[2].split("-")[3].to_i - 1]
 			
@@ -70,39 +70,21 @@ def find_optimal_move(board_state)
 	end
 
 	# keep it from getting stuck
+	# TODO check for case where you can only move up
 	return (if Random.rand > 0.5 then :arrow_right else :arrow_left end)
 
 end
 
 # initialize selenium
-driver = Driver.new "http://gabrielecirulli.github.io/2048/"
-#driver = Driver.new "http://doge2048.com"
+# driver = Driver.new "file://C:/Users/Colin Rood/Code/2048-master/index.html"
+# driver = Driver.new "http://gabrielecirulli.github.io/2048/"
+driver = Driver.new "http://doge2048.com"
 
 # 4x4 matrix to hold board state
 tile_matrix = driver.read_tiles
 
-RubyProf.start
-
 # loop an artibrary number of times
-# will eventually go until it loses (or wins)
+# TODO go until it loses (or wins)
 150.times do
-	# store board state
-	tile_list = driver.get_elements_by_class "tile"
-	
-	# move
 	driver.send_keys(find_optimal_move(driver.read_tiles))
-
-	# only proceed once the DOM updates
-	# prevents crashes from accessing unavailable elements
-	i = 0
-	while (tile_list == driver.get_elements_by_class("tile") && i < 3)
-		i += 1
-	end
-	print i.to_s + " "
 end
-
-profile = RubyProf.stop
-printer = RubyProf::FlatPrinter.new(profile)
-printer.print(STDOUT)
-
-#driver.send_keys(:arrow_left)
